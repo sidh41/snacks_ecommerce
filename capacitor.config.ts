35a6bdb75425637a53679dc345c0e13a5fe9b411
Siddhesh Shedge <eights41@gmail.com>
2023-02-07 11:09:23 +0530

    First Commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'snacks_ecommerce',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
